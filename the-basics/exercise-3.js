40c60971b5400a1f7c58f1569c152fb4a5262f84
chipper111 <chipperfielding@gmail.com>
2022-12-01 06:26:54 -0500

    exercise 3 completed

---
+++ the-basics/exercise-3.js
@@ -0,0 +1,19 @@
+/*
+Exercise 3 
+12.01.22
+
+Identify the data type for each of the following values:
+    'true'
+    false
+    1.5
+    2
+    undefined
+    { foo: 'bar' }
+*/
+
+console.log(typeof "true");
+console.log(typeof false);
+console.log(typeof 1.5);
+console.log(typeof 2);
+console.log(typeof undefined);
+console.log(typeof { foo: "bar" });
