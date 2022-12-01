0a2e50f15f502aa8985518073e67b9559f784250
chipper111 <chipperfielding@gmail.com>
2022-12-01 07:07:38 -0500

    exercise 12 completed

---
+++ the-basics/exercise-12.js
@@ -0,0 +1,12 @@
+/*
+Exercise 12
+12.01.22
+
+What value does the following expression evaluate to?
+
+'12' < '9'
+*/
+
+console.log("12" < "9");
+// evaluates to true b/c it lexically compares both strings starting with the 1 vs 9
+// 1 lexically comes before 9 in terms of the related ascii codes
