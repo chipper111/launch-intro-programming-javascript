be656ab02b978536174718ce07ab071e01ab1a14
chipper111 <chipperfielding@gmail.com>
2022-12-01 06:36:11 -0500

    exercise 5 completed

---
+++ the-basics/exercise-5.js
@@ -0,0 +1,13 @@
+/*
+Exercise 5
+12.01.22
+
+Refactor the code from the previous exercise to use explicit coercion, so it logs 15 instead.
+
+previous code below => 
+
+console.log('5' + 10);
+
+*/
+
+console.log(Number("5") + 10);
