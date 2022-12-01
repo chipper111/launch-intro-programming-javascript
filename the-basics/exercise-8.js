e0bc1c181eed3a7c59ae6bb8b34a3571344e19f8
chipper111 <chipperfielding@gmail.com>
2022-12-01 06:51:50 -0500

    exercise 8 completed

---
+++ the-basics/exercise-8.js
@@ -0,0 +1,16 @@
+/*
+Exercise 8
+12.01.22
+
+Create an array named names that contains a list of pet names. For instance:
+Name
+asta
+butterscotch
+pudding
+neptune
+darwin
+
+You can use that data or make up your own.
+*/
+
+const names = ["Joey", "Peanut", "Berty", "Ringo"];
