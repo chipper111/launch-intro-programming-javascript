11764c845d62af22491e1717d5681faa9f796aca
chipper111 <chipperfielding@gmail.com>
2022-12-01 07:04:41 -0500

    exercise 11 completed

---
+++ the-basics/exercise-11.js
@@ -0,0 +1,12 @@
+/*
+Exercise 11
+12.01.22
+
+What value does the following expression evaluate to?
+
+parseInt('3.1415')
+*/
+
+console.log(parseInt("3.1415"));
+//evaluates to 3 b/c parseInt only takes everything to the left of the decimal point
+// and truncates everything to the left of the decimal point
