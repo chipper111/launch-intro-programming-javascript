08407661f91c4e283a2266f49045ac6692d699e8
chipper111 <chipperfielding@gmail.com>
2022-12-02 05:56:00 -0500

    input/output examples

---
+++ input-output/personalized-greeting.js
@@ -0,0 +1,8 @@
+/*
+Input/Ouput Chapter Example
+12.02.22
+*/
+
+let rlSync = require("readline-sync");
+let name = rlSync.question(`What's your name?\n`);
+console.log(`Good Morning, ${name}!`);
---
+++ input-output/sum-numbers.js
@@ -0,0 +1,12 @@
+/*
+Input/Ouput Example: Add Two Numbers Together
+12.02.22
+*/
+
+let rlSync = require("readline-sync");
+
+let num1 = Number(rlSync.question(`Enter the first number\n`));
+let num2 = Number(rlSync.question(`Enter the second number\n`));
+let sum = num1 + num2;
+
+console.log(`The numbers ${num1} and ${num2} add to ${sum}.`);
