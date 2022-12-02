2d7ad53569a37fde53bf1609ae2b9f278b3a1170
chipper111 <chipperfielding@gmail.com>
2022-12-02 05:17:58 -0500

    exercise 1 completed

---
+++ variables/greeter.js
@@ -0,0 +1,17 @@
+/*
+Exercise 1
+12.02.22
+ 
+Write a program named greeter.js that greets 'Victor' three times. Your program should use a variable and not hard code the string value 'Victor' in each greeting. Here's an example run of the program:
+
+$ node greeter.js
+Good Morning, Victor.
+Good Afternoon, Victor.
+Good Evening, Victor.
+*/
+
+const name = "Victor";
+
+console.log(`Good Morning, ${name}.`);
+console.log(`Good Afternoon, ${name}.`);
+console.log(`Good Evening, ${name}.`);
